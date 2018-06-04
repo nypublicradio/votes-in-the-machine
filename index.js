be20ace9d27a12ac13b60c8018ec30984d5e929b
Brian Whitton <me@brianwhitton.com>
2018-06-04 19:00:29 -0400

    draft handler

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-exports.handler = event => {
-  console.log('test event:', event);
+const APClient = require('./lib/ap-client');
+const { uploadToS3 } = require('./lib/upload');
+const moment = require('moment');
+
+exports.handler = async params => {
+  const ap = new APClient(params);
+  const { race } = params;
+
+  let nextrequest = await ap.nextRequest();
+
+  let currentResults;
+  if (nextrequest) {
+    currentResults = await ap.mergeResults(nextrequest);
+  } else {
+    currentResults = await ap.fetchResults();
+  }
+
+  let response = await uploadToS3(currentResults, {prefix: race});
+  console.log('success:', response);
 }
