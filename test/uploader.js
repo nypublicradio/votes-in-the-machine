12c5c7331b8da35a4fcd02199b050c32f8aef630
Brian Whitton <me@brianwhitton.com>
2018-06-05 17:46:40 -0400

    upload timestamped results into timestamp directory

--- test/uploader.js
+++ test/uploader.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 const { uploadToS3 } = require('../lib/upload');
 
 describe('uploader', function() {
-  
+
   it('uploads to a timestamped path and a static path', async function() {
     let now = moment();
     let putSpy = AWS.mock('S3', 'putObject');
@@ -18,7 +18,7 @@ describe('uploader', function() {
     let staticCall = putSpy.stub.secondCall;
 
     assert.ok(putSpy.stub.calledTwice);
-    assert.equal(dynamicCall.args[0].Key, `foo/${now.format('YYYY-MM-DD_HH-mm-ss')}.json`, 'static call');
+    assert.equal(dynamicCall.args[0].Key, `foo/timestamp/${now.format('YYYY-MM-DD_HH-mm-ss')}.json`, 'static call');
     assert.equal(staticCall.args[0].Key, 'foo/results.json', 'static call');
   });
 
