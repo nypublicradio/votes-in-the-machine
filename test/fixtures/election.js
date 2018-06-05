3be866b0552a51987f2697dadcd1575c003d8a00
Brian Whitton <me@brianwhitton.com>
2018-06-05 14:26:07 -0400

    add election results fixture

---
+++ test/fixtures/election.js
@@ -0,0 +1,282 @@
+module.exports = {
+  "electionDate": "2018-06-05",
+  "timestamp": "2018-06-05T12:43:42.999Z",
+  "races": [{
+      "test": false,
+      "raceID": "31206",
+      "raceType": "Primary",
+      "raceTypeID": "R",
+      "officeID": "H",
+      "officeName": "U.S. House",
+      "description": "South, Atlantic City",
+      "party": "GOP",
+      "seatName": "District 2",
+      "seatNum": "2",
+      "national": true,
+      "reportingUnits": [{
+        "statePostal": "NJ",
+        "stateName": "New Jersey",
+        "level": "state",
+        "lastUpdated": "2018-06-05T04:06:58.050Z",
+        "precinctsReporting": 0,
+        "precinctsTotal": 523,
+        "precinctsReportingPct": 0,
+        "candidates": [{
+            "first": "Sam",
+            "last": "Fiocchi",
+            "party": "GOP",
+            "candidateID": "39132",
+            "polID": "66835",
+            "ballotOrder": 1,
+            "polNum": "34353",
+            "voteCount": 0
+          },
+          {
+            "first": "Seth",
+            "last": "Grossman",
+            "abbrv": "Grossmn",
+            "party": "GOP",
+            "candidateID": "39131",
+            "polID": "63124",
+            "ballotOrder": 2,
+            "polNum": "34613",
+            "voteCount": 0
+          },
+          {
+            "first": "Hirsh",
+            "last": "Singh",
+            "party": "GOP",
+            "candidateID": "39134",
+            "polID": "65859",
+            "ballotOrder": 3,
+            "polNum": "34962",
+            "voteCount": 0
+          },
+          {
+            "first": "Robert",
+            "last": "Turkavage",
+            "abbrv": "Turkavg",
+            "party": "GOP",
+            "candidateID": "39133",
+            "polID": "62567",
+            "ballotOrder": 4,
+            "polNum": "34532",
+            "voteCount": 0
+          }
+        ]
+      }]
+    },
+    {
+      "test": false,
+      "raceID": "31214",
+      "raceType": "Primary",
+      "raceTypeID": "R",
+      "officeID": "H",
+      "officeName": "U.S. House",
+      "description": "Northern Border",
+      "party": "GOP",
+      "seatName": "District 5",
+      "seatNum": "5",
+      "national": true,
+      "reportingUnits": [{
+        "statePostal": "NJ",
+        "stateName": "New Jersey",
+        "level": "state",
+        "lastUpdated": "2018-06-05T04:06:58.050Z",
+        "precinctsReporting": 0,
+        "precinctsTotal": 493,
+        "precinctsReportingPct": 0,
+        "candidates": [{
+            "first": "Steve",
+            "last": "Lonegan",
+            "party": "GOP",
+            "candidateID": "39143",
+            "polID": "57692",
+            "ballotOrder": 1,
+            "polNum": "34750",
+            "voteCount": 0
+          },
+          {
+            "first": "John",
+            "last": "McCann",
+            "party": "GOP",
+            "candidateID": "39144",
+            "polID": "66840",
+            "ballotOrder": 2,
+            "polNum": "35181",
+            "voteCount": 0
+          }
+        ]
+      }]
+    },
+    {
+      "test": false,
+      "raceID": "31222",
+      "raceType": "Primary",
+      "raceTypeID": "R",
+      "officeID": "H",
+      "officeName": "U.S. House",
+      "description": "North Central",
+      "party": "GOP",
+      "seatName": "District 7",
+      "seatNum": "7",
+      "national": true,
+      "reportingUnits": [{
+        "statePostal": "NJ",
+        "stateName": "New Jersey",
+        "level": "state",
+        "lastUpdated": "2018-06-05T04:06:58.050Z",
+        "precinctsReporting": 0,
+        "precinctsTotal": 639,
+        "precinctsReportingPct": 0,
+        "candidates": [{
+            "first": "Lindsay",
+            "last": "Brown",
+            "party": "GOP",
+            "candidateID": "39147",
+            "polID": "66842",
+            "ballotOrder": 2,
+            "polNum": "35183",
+            "voteCount": 0
+          },
+          {
+            "first": "Leonard",
+            "last": "Lance",
+            "party": "GOP",
+            "incumbent": true,
+            "candidateID": "39111",
+            "polID": "56743",
+            "ballotOrder": 3,
+            "polNum": "31504",
+            "voteCount": 0
+          },
+          {
+            "first": "Raafat",
+            "last": "Barsoom",
+            "party": "GOP",
+            "candidateID": "39148",
+            "polID": "66880",
+            "ballotOrder": 1,
+            "polNum": "34570",
+            "voteCount": 0
+          }
+        ]
+      }]
+    },
+    {
+      "test": false,
+      "raceID": "31231",
+      "raceType": "Primary",
+      "raceTypeID": "R",
+      "officeID": "H",
+      "officeName": "U.S. House",
+      "description": "North Central, Morris Co",
+      "party": "GOP",
+      "seatName": "District 11",
+      "seatNum": "11",
+      "national": true,
+      "reportingUnits": [{
+        "statePostal": "NJ",
+        "stateName": "New Jersey",
+        "level": "state",
+        "lastUpdated": "2018-06-05T04:06:58.050Z",
+        "precinctsReporting": 0,
+        "precinctsTotal": 576,
+        "precinctsReportingPct": 0,
+        "candidates": [{
+            "first": "Patrick",
+            "last": "Allocco",
+            "party": "GOP",
+            "candidateID": "39159",
+            "polID": "66848",
+            "ballotOrder": 1,
+            "polNum": "35189",
+            "voteCount": 0
+          },
+          {
+            "first": "Peter",
+            "last": "De Neufville",
+            "abbrv": "DeNefvll",
+            "party": "GOP",
+            "candidateID": "39162",
+            "polID": "66851",
+            "ballotOrder": 2,
+            "polNum": "35192",
+            "voteCount": 0
+          },
+          {
+            "first": "Antony",
+            "last": "Ghee",
+            "party": "GOP",
+            "candidateID": "39160",
+            "polID": "66849",
+            "ballotOrder": 3,
+            "polNum": "35190",
+            "voteCount": 0
+          },
+          {
+            "first": "Martin",
+            "last": "Hewitt",
+            "party": "GOP",
+            "candidateID": "39161",
+            "polID": "66850",
+            "ballotOrder": 4,
+            "polNum": "35191",
+            "voteCount": 0
+          },
+          {
+            "first": "Jay",
+            "last": "Webber",
+            "party": "GOP",
+            "candidateID": "39158",
+            "polID": "66847",
+            "ballotOrder": 5,
+            "polNum": "34088",
+            "voteCount": 0
+          }
+        ]
+      }]
+    },
+    {
+      "test": false,
+      "raceID": "31568",
+      "raceType": "Primary",
+      "raceTypeID": "R",
+      "officeID": "S",
+      "officeName": "U.S. Senate",
+      "party": "GOP",
+      "national": true,
+      "reportingUnits": [{
+        "statePostal": "NJ",
+        "stateName": "New Jersey",
+        "level": "state",
+        "lastUpdated": "2018-06-05T04:06:58.050Z",
+        "precinctsReporting": 0,
+        "precinctsTotal": 6346,
+        "precinctsReportingPct": 0,
+        "candidates": [{
+            "first": "Brian",
+            "last": "Goldberg",
+            "party": "GOP",
+            "candidateID": "39124",
+            "polID": "63446",
+            "ballotOrder": 1,
+            "polNum": "34740",
+            "voteCount": 0
+          },
+          {
+            "first": "Bob",
+            "last": "Hugin",
+            "party": "GOP",
+            "candidateID": "39122",
+            "polID": "66858",
+            "ballotOrder": 2,
+            "polNum": "35168",
+            "voteCount": 0
+          }
+        ]
+      }]
+    }
+  ],
+  "nextrequest": "http://api.ap.org/v2/elections/2018-06-05?format=JSON&party=GOP&statepostal=NJ&uncontested=FALSE&minDateTime=2018-06-05T04%3a06%3a58.050Z"
+};
